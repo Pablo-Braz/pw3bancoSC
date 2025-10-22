e8a6a687021a803f65a7689a3d9d8fe6dd7ecf72
pablogfgdgf <pbraz0460@gmail.com>
2025-10-22 19:52:13 -0300

    update

--- api/src/app.js
+++ api/src/app.js
@@ -1,52 +1,34 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import usuarioRoutes from './routes/usuarioRoutes.js';
+import instituicoesRoutes from './routes/instituicoesRoutes.js';
+import instituicaoUsuarioRoutes from './routes/InstituicaoUsuarioRoutes.js';
 dotenv.config();
-import cookieParser from 'cookie-parser';
 
-// Importando as rotas
-import veiculoRoute from './routes/veiculoRoute.js';
-import usuarioRoute from './routes/usuario.js';
-import autenticacaoRoute from './routes/autenticacao.js';
 
 const app = express();
 
-app.use(cors({
-    origin: (origin, cb) => cb(null, origin || true),
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
 
 
-// Rotas de públicas
+// Rotas públicas
 app.get('/',(req,res)=>{
     const rootDomain = req.protocol + '://' + req.get('host');
     res.status(200).json({     
         status_server: 'ok',
         dominio_raiz : rootDomain,
-        atualização: '17/09/2025 - 14:30',
+        atualização: '14/09/2024 - 18:42',
         rotas:{
-            'POST - Login': `${rootDomain}/api/usuario/login`,
-            'POST - Cadastrar usuário': `${rootDomain}/api/usuario`,
-            'GET - Usuário logado': `${rootDomain}/api/usuario/logado`,
-            'GET - Listar usuários': `${rootDomain}/api/usuario`,
-            'GET - Consultar usuário por ID': `${rootDomain}/api/usuario/{id}`,
-            'PUT - Editar usuário': `${rootDomain}/api/usuario/{id}`,
-            'DELETE - Deletar usuário': `${rootDomain}/api/usuario/{id}`,
-            'GET - Consultar veículo por ID': `${rootDomain}/api/veiculo/{id}`,
-            'GET - Consultar todos os veículos': `${rootDomain}/api/veiculos`,
-            'POST - Cadastrar veículo':`${rootDomain}/api/veiculo`,
-            'PUT - Editar veículo': `${rootDomain}/api/veiculo/{id}`,
-            'DELETE - Deletar veículo': `${rootDomain}/api/veiculo/{id}`
+            'GET - teste': `${rootDomain}/api/teste/teste`
         }
     });
 });
 
-// Configurando as rotas
-app.use('/api', autenticacaoRoute);
-app.use('/api', usuarioRoute);
-app.use('/api', veiculoRoute);
+app.use('/', usuarioRoutes);
+app.use('/', instituicoesRoutes);
+app.use('/', instituicaoUsuarioRoutes);
 
 const PORT = process.env.PORT || 3000; 
 app.listen(PORT,()=>{
